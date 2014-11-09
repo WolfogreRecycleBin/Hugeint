9ee85bef6404df569dc256fe824a39dd5e34619e
wolfogre <wolfogre@qq.com>
2014-11-09 23:17:14 +0800

    构造函数与插入结点函数设计受阻

--- Hugeint.cpp
+++ Hugeint.cpp
@@ -2,7 +2,9 @@
 #include "Hugeint.h"
 #include <iostream>
 using namespace std;
-Hugeint::Hugeint(long long num, unsigned int rank)
+
+
+Hugeint::Hugeint(long long num, unsigned int rank)//ToDo
 {
 	if(num>=0) sign=1;
 	else
@@ -16,4 +18,35 @@ Hugeint::Hugeint(long long num, unsigned int rank)
 Hugeint::~Hugeint()
 {
 	//ToDo
+}
+
+void Hugeint::Insert(int num, unsigned int rank)//ToDo
+{
+	Node *p=NULL;
+	for(p=head;p->next!=NULL;p=p->next);					//pÖ¸ÕëÒÆÏòÁ´±íÄ©Î²
+	}
+	}
+	// if(rank==0)
+	// {
+	// 	if(head) head->n=num;
+	// 	else
+	// 	{
+	// 		head=new Node;
+	// 		head->r=0;
+	// 		head->n=num;
+	// 		head->next=head->prior=NULL;
+	// 	}
+	// 	return;
+	// }
+	// else
+	// {
+	// 	Node *p=head;
+	// 	while(p)
+	// 	{
+	// 		if(p->r<rank) p=p->next;
+	// 		else break; 
+	// 	}
+	// 	if(p) p->n=num;
+	// 	else
+	// }
 }
--- Hugeint.h
+++ Hugeint.h
@@ -6,49 +6,54 @@
 using namespace std;
 struct Node
 {
-	int r;													//阶数
+	unsigned int r;											//阶数
 	int n;													//值
 	Node *prior,*next;										//前指针、后指针
 };
 class Hugeint
 {
 public:
-	Hugeint(long long num=0, unsigned int rank=0);
-	Hugeint(char str[10000]="0");
-	Hugeint & operator=(const Hugeint hi);
-	virtual ~Hugeint();
+	//构造与析构
+	Hugeint(long long num=0, unsigned int rank=0);//ToDo
+	Hugeint(char str[10000]="0");//ToDo
+	Hugeint & operator=(const Hugeint hi);//ToDo
+	virtual ~Hugeint();//ToDo
 	//重载方括号运算符
-	int & operator[](int index);
+	int & operator[](int index);//ToDo
 	//重载取负运算符
-	Hugeint operator-();
+	Hugeint operator-();//ToDo
 	//重载算术运算符
-	friend Hugeint operator+(const Hugeint hi1, const Hugeint hi2);
-	friend Hugeint operator-(const Hugeint hi1, const Hugeint hi2);
-	friend Hugeint operator*(const Hugeint hi1, const Hugeint hi2);
-	friend Hugeint operator/(const Hugeint hi1, const Hugeint hi2);
-	friend Hugeint operator%(const Hugeint hi1, const Hugeint hi2);
+	friend Hugeint operator+(const Hugeint hi1, const Hugeint hi2);//ToDo
+	friend Hugeint operator-(const Hugeint hi1, const Hugeint hi2);//ToDo
+	friend Hugeint operator*(const Hugeint hi1, const Hugeint hi2);//ToDo
+	friend Hugeint operator/(const Hugeint hi1, const Hugeint hi2);//ToDo
+	friend Hugeint operator%(const Hugeint hi1, const Hugeint hi2);//ToDo
 	//重载迭代赋值运算符
-	Hugeint & operator+=(const Hugeint hi);
-	Hugeint & operator-=(const Hugeint hi);
-	Hugeint & operator*=(const Hugeint hi);
-	Hugeint & operator/=(const Hugeint hi);
-	Hugeint & operator%=(const Hugeint hi);
+	Hugeint & operator+=(const Hugeint hi);//ToDo
+	Hugeint & operator-=(const Hugeint hi);//ToDo
+	Hugeint & operator*=(const Hugeint hi);//ToDo
+	Hugeint & operator/=(const Hugeint hi);//ToDo
+	Hugeint & operator%=(const Hugeint hi);//ToDo
 	//重载前后增减量运算符
-	Hugeint & operator++();
-	Hugeint & operator--();
-	Hugeint operator++(int);
-	Hugeint operator--(int);
+	Hugeint & operator++();//ToDo
+	Hugeint & operator--();//ToDo
+	Hugeint operator++(int);//ToDo
+	Hugeint operator--(int);//ToDo
 	//重载关系运算
-	friend bool operator==(const Hugeint &hi1, const Hugeint &hi2);
-	friend bool operator!=(const Hugeint &hi1, const Hugeint &hi2);
-	friend bool operator> (const Hugeint &hi1, const Hugeint &hi2);
-	friend bool operator>=(const Hugeint &hi1, const Hugeint &hi2);
-	friend bool operator< (const Hugeint &hi1, const Hugeint &hi2);
-	friend bool operator<=(const Hugeint &hi1, const Hugeint &hi2);
+	friend bool operator==(const Hugeint &hi1, const Hugeint &hi2);//ToDo
+	friend bool operator!=(const Hugeint &hi1, const Hugeint &hi2);//ToDo
+	friend bool operator> (const Hugeint &hi1, const Hugeint &hi2);//ToDo
+	friend bool operator>=(const Hugeint &hi1, const Hugeint &hi2);//ToDo
+	friend bool operator< (const Hugeint &hi1, const Hugeint &hi2);//ToDo
+	friend bool operator<=(const Hugeint &hi1, const Hugeint &hi2);//ToDo
 	//重载I/O流运算符
-	friend ostream & operator<<(ostream &out, const Hugeint &hi);
-	friend istream & operator>>(istream &in, Hugeint &hi);
-
+	friend ostream & operator<<(ostream &out, const Hugeint &hi);//ToDo
+	friend istream & operator>>(istream &in, Hugeint &hi);//ToDo
+protected:
+	void Insert(Node & nd, unsigned int rank);//ToDo			//将nd插入到rank阶数应该存在的位置，可增、改
+	void Delete(Node & nd);	//ToDo							//删除结点
+	Node & Find(unsigned int rank=0);//ToDo						//寻找返回rank阶数
+	void FixList();//ToDo											//修正链表
 private:
 	int sign;												//1表示正数，-1表示负数
 	Node *head;
